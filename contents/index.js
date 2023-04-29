c779a44dfa73930aebc1382ed869859dcc64ed20
Rafael <fc56891@alunos.fc.ul.pt>
2023-04-29 02:30:03 +0100

    Tabuleiro com utilizador controlável

--- contents/index.js
+++ contents/index.js
@@ -1,11 +1,76 @@
 /// <reference path="p5/p5.global-mode.d.ts" />
 
-function setup() {
-    createCanvas(300,300).parent('canvas');
+// variaveis
+let xPos = 0; // x inicial
+let yPos = 0; // y inicial
+const squareSize = 40; // size of each square on the board
 
+ // arrays 2D (0=caminhavel, 1=bloqueado)
+const level1 = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+    [1, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+    [1, 1, 1, 0, 1, 1, 1, 0, 1, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
+];
+
+let board = level1;
+    
+
+function setup() {
+    createCanvas(400,400).parent('canvas');
 }
 
 function draw() {
-    background('blue');
+    background('brown');
+
+    // Draw board
+    stroke(0); // set stroke color to black
+    strokeWeight(2); // set stroke weight to 2 pixels
+    for (let i = 0; i < 10; i++) { // loop through rows
+        for (let j = 0; j < 10; j++) { // loop through columns
+            if (board[i][j] === 0) {
+                fill(100,0,20)
+                noStroke()
+                rect(j*squareSize, i*squareSize, squareSize, squareSize); // draw a square at (j*squareSize, i*squareSize) with dimensions (squareSize, squareSize)
+            }
+        }
+    }
+
+    // Draw character
+    fill(255, 33, 0); // set fill color to red
+    rect(xPos*squareSize, yPos*squareSize, squareSize, squareSize); // draw a square at (xPos*squareSize, yPos*squareSize) with dimensions (squareSize, squareSize)
 
 }
+
+// Handle arrow key input
+function keyPressed() {
+    let newXPos = xPos; // initialize newXPos to current xPos
+    let newYPos = yPos; // initialize newYPos to current yPos
+    if (keyCode === UP_ARROW) { // if up arrow is pressed
+        newYPos--; // move up one row
+    } else if (keyCode === DOWN_ARROW) { // if down arrow is pressed
+        newYPos++; // move down one row
+    } else if (keyCode === LEFT_ARROW) { // if left arrow is pressed
+        newXPos--; // move left one column
+    } else if (keyCode === RIGHT_ARROW) { // if right arrow is pressed
+        newXPos++; // move right one column
+    }
+    // Check if new position is within bounds and unblocked
+    if (newXPos >= 0 && newXPos < board[0].length && newYPos >= 0 && newYPos < board.length && board[newYPos][newXPos] === 0) {
+        xPos = newXPos; // update xPos
+        yPos = newYPos; // update yPos
+    }
+}
+
+// Listen for keyboard events on window
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === LEFT_ARROW || event.keyCode === RIGHT_ARROW || event.keyCode === UP_ARROW || event.keyCode === DOWN_ARROW) {
+        event.preventDefault(); // prevent default behavior of arrow keys (scrolling page)
+    }
+  });
